7b5ec3768c9191d514962cc53dc3528f1e338a7a
kaue-souza7 <kauekaue960@gmail.com>
2024-09-20 11:59:38 -0300

    feat(Class CorpoHumano); Incorporando construtor getter e setters.

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -2,4 +2,47 @@ package main.java;
 
 public class CorpoHumano {
 
+    private double altura;
+    private double volume;
+    private int densidade;
+    private int massa;
+
+
+    public CorpoHumano(int massa, double volume, int densidade, double altura) {
+        this.altura = altura;
+        this.volume = volume;
+        this.densidade = densidade;
+        this.massa = massa;
+    }
+
+    public double calcularIMC(){
+        return massa / (altura*altura);
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getVolume() {
+        return volume;
+    }
+    public void setVolume(double peso) {
+        this.volume = peso;
+    }
+    public int getDensidade() {
+        return densidade;
+    }
+    public void setDensidade(int densidade) {
+        this.densidade = densidade;
+    }
+    public int getMassa() {
+        return massa;
+    }
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+
+
 }
